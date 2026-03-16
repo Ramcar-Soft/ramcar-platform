dad96be724ed75d8bc76b6c19bf720dad9710925
Ivan Eusebio <ivaneusebioperez@gmail.com>
2026-03-16 17:30:13 -0600

    chore: add claude permissions

--- apps/desktop/dist-electron/main.js
+++ apps/desktop/dist-electron/main.js
@@ -1,32 +1,30 @@
-import { app as n, BrowserWindow as t } from "electron";
-import { createRequire as a } from "node:module";
+import { app as n, BrowserWindow as i } from "electron";
 import { fileURLToPath as c } from "node:url";
 import o from "node:path";
-a(import.meta.url);
-const r = o.dirname(c(import.meta.url));
-process.env.APP_ROOT = o.join(r, "..");
-const i = process.env.VITE_DEV_SERVER_URL, _ = o.join(process.env.APP_ROOT, "dist-electron"), s = o.join(process.env.APP_ROOT, "dist");
-process.env.VITE_PUBLIC = i ? o.join(process.env.APP_ROOT, "public") : s;
+const t = o.dirname(c(import.meta.url));
+process.env.APP_ROOT = o.join(t, "..");
+const s = process.env.VITE_DEV_SERVER_URL, R = o.join(process.env.APP_ROOT, "dist-electron"), r = o.join(process.env.APP_ROOT, "dist");
+process.env.VITE_PUBLIC = s ? o.join(process.env.APP_ROOT, "public") : r;
 let e;
 function l() {
-  e = new t({
+  e = new i({
     icon: o.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
     webPreferences: {
-      preload: o.join(r, "preload.mjs")
+      preload: o.join(t, "preload.mjs")
     }
   }), e.webContents.on("did-finish-load", () => {
     e == null || e.webContents.send("main-process-message", (/* @__PURE__ */ new Date()).toLocaleString());
-  }), i ? e.loadURL(i) : e.loadFile(o.join(s, "index.html"));
+  }), s ? e.loadURL(s) : e.loadFile(o.join(r, "index.html"));
 }
 n.on("window-all-closed", () => {
   process.platform !== "darwin" && (n.quit(), e = null);
 });
 n.on("activate", () => {
-  t.getAllWindows().length === 0 && l();
+  i.getAllWindows().length === 0 && l();
 });
 n.whenReady().then(l);
 export {
-  _ as MAIN_DIST,
-  s as RENDERER_DIST,
-  i as VITE_DEV_SERVER_URL
+  R as MAIN_DIST,
+  r as RENDERER_DIST,
+  s as VITE_DEV_SERVER_URL
 };
