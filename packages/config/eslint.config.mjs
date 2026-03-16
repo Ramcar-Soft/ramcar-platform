a277adf0abcd43b2ed9cca016582d0f4248ea70c
Ivan Eusebio <ivaneusebioperez@gmail.com>
2026-03-16 16:45:06 -0600

    chore: add packages/config with tsconfigs, eslint, prettier, tailwind preset

---
+++ packages/config/eslint.config.mjs
@@ -0,0 +1,19 @@
+import tseslint from "typescript-eslint";
+import prettierConfig from "eslint-config-prettier";
+
+export default tseslint.config(
+  {
+    ignores: ["**/dist/**", "**/.next/**", "**/node_modules/**", "**/out/**"],
+  },
+  ...tseslint.configs.recommended,
+  prettierConfig,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  }
+);
