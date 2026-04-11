d5faa28bdef131cbbd87ffa13ddb714a2a93dbfe
Ivan Eusebio <ivaneusebioperez@gmail.com>
2026-04-10 18:52:07 -0600

    fix: children prop issue

--- apps/web/src/shared/components/theme-provider.tsx
+++ apps/web/src/shared/components/theme-provider.tsx
@@ -1,8 +1,10 @@
 "use client";
-
+import { ReactNode} from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import type { ThemeProviderProps } from "next-themes";
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+type ThemeProviderPropsWithChildren = ThemeProviderProps & { children: ReactNode };
+
+export function ThemeProvider({ children, ...props }: ThemeProviderPropsWithChildren) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
