7284d2ced906f0bd6dce94adfceb7a4238c2bb28
Ivan Eusebio <ivaneusebioperez@gmail.com>
2026-04-24 01:34:08 -0600

    Fix/cors dev (#32)
    * fix: cors missing url
    
    * chore: add missing vercel domains

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,7 +5,17 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ["http://localhost:3000", "http://localhost:5173"],
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:5173",
+      "https://ramcar-platform-dev.up.railway.app",
+      "https://dev.ramcarsoft.com",
+      "dev.ramcarsoft.com",
+      "https://app.ramcarsoft.com",
+      "app.ramcarsoft.com",
+      "ramcarsoft-web.vercel.app",
+      "https://ramcarsoft-web.vercel.app"
+    ],
     credentials: true,
   });
 
