0c3afc5e8d1399f5813af446c86ba2d38139f046
Ivan Eusebio <ivaneusebioperez@gmail.com>
2026-04-24 01:31:48 -0600

    fix: cors missing url

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,7 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ["http://localhost:3000", "http://localhost:5173"],
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:5173",
+      "https://ramcar-platform-dev.up.railway.app"
+    ],
     credentials: true,
   });
 
